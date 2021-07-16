b0104d847bce02ed2bf7af4f2c10ef0ca60641be
Rytis Alekna <rytis.a@simplex.com>
2021-07-16 12:25:05 +0300

    Added ability convert JSON to CSV

--- README.md
+++ README.md
@@ -1,12 +1,24 @@
 # Translations converter
 
-This is a utility to convert translations CSV to JSON. CSV is used by our translators.
+This is a utility to convert translations CSV to JSON and back. CSV is used by our translators.
 
 ## Params
 `to-json`
 
 ## Example usage
+
+### To convert CSV to JSON 
 ```bash
 npm i
-node src/index.js to-json < example-source-files/sample-translations_es-LA.csv > output-translations.json
+node src/index.js to-json < example-source-files/sample-translations_es-LA.csv > example-source-files/sample-translations_es-LA.csv.json
 ```
+
+### To convert JSON to CSV 
+```bash
+npm i
+node src/index.js to-csv < example-source-files/sample-translations_es-LA.csv.json > example-source-files/sample-translations_es-LA.csv
+```
+
+### Note
+
+To avoid line break in last JSON field value end, delete trailing line in CVS before parsing.
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const fs = require("fs");
-const csvParser = require("csv-parser");
 const Papa = require("papaparse");
 const { unflatten } = require("flat");
+const flatten = require("flat");
 const stdinBuffer = fs.readFileSync(0); // STDIN_FILENO = 0
-// console.log(`process.argv`, process.execArgv, process.argv0,);
-if (process.argv[2] === "to-json") {
-    console.log(JSON.stringify(unflatten(Object.fromEntries(Papa.parse(stdinBuffer.toString()).data)), null, 2));
-}
 
-// console.log(stdinBuffer.toString());
+if (process.argv[2] === "to-json") {
+    console.log(
+        JSON.stringify(
+            unflatten(
+                Object.fromEntries(
+                    Papa.parse(stdinBuffer.toString(), {skipEmptyLines: true, greedy: false}).data
+                )
+            ), null, 2)
+    );
+} else if (process.argv[2] === "to-csv") {
+    console.log(
+      Papa.unparse(Object.entries(flatten(JSON.parse(stdinBuffer.toString()))))
+    );
+}
